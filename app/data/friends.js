7fb77f541143a732d4c340e9b264b27916dac6bf
Griffin <griffincriste@gmail.com>
2019-07-24 15:53:01 -0400

    added some functionality

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friendsArray = [
+
+  {
+    name:"Griffin",
+    photo:"",
+    scores:[5,1,4,4,5,1,2,5,4,1]
+  },{
+    name:"Sarah",
+    photo:"",
+    scores:[1,2,3,4,2,3,4,2,3,2]
+  },{
+    name:"Maggie",
+    photo:"",
+    scores:[2,3,4,3,2,1,5,2,2,3]
+  },{
+    name:"Bob",
+    photo:"",
+    scores:[2,1,5,4,3,1,2,3,2,5]
+  },{
+    name:"Jonathon",
+    photo:"",
+    scores:[1,1,3,3,5,4,1,3,4,5]
+  },{
+    name:"Mr.Magoo",
+    photo:"",
+    scores:[1,1,1,4,5,4,5,5,3,5]
+  },{
+    name:"Jack",
+    photo:"",
+    scores:[2,1,3,4,5,2,2,3,5,2]
+  }
+]
+  
+
+
+
+module.exports = friendsArray;
